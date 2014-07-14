29fbaaaa12f030453a572920a717870549f53fdd
Raymond Zeng <raymondzeng@live.com>
2014-07-14 14:17:12 -0400

    alphabeta pruning. if ai already lost, it chooses left to bottom but doesn't prolong

--- python/minmax_tictactoe.py
+++ python/minmax_tictactoe.py
@@ -1,23 +1,51 @@
 # an implementation for tictactoe AI using minmax and alphabeta pruning
-# board is col row array
+# board array access is board[col][row]
 
 # boards are always squares
-def make_board(xy):
+def make_board(width):
     board = []
-    for i in xrange(xy):
-        board.append(["_"] * xy)
+    for i in xrange(width):
+        board.append(["_"] * width)
     return board
         
+# board is a list of columns and we want to print by row
 def print_board(board):
     for i in xrange(len(board)):
         row = [board[j][i] for j in xrange(len(board))]
         print " | ".join(row)
         
-def new_state(old, player, (y,x)):
+# create a new board but with position @(col, row) changed to @player
+def new_state(old, player, (col,row)):
     import copy
     new = copy.deepcopy(old)
-    new[y][x] = player
+    new[col][row] = player
     return new
+
+def get_winner(board):
+    # look for vertical win
+    for col in board:
+        if len(set(col)) == 1 and col[0] != "_":
+            return col[0]
+            
+    # horizontal win
+    d = len(board)
+    rows = [[board[i][x] for i in xrange(d)] for x in xrange(d)]
+    for row in rows:
+        if len(set(row)) == 1 and row[0] != "_":
+            return row[0]
+            
+    # diagonal win: top left to bottom right
+    tl_br_diag = [board[i][i] for i in xrange(d)]
+    if len(set(tl_br_diag)) == 1 and tl_br_diag[0] != "_":
+        return tl_br_diag[0]
+
+    # bottom left to top right
+    bl_tr_diag = [board[i][j] for (i,j) in zip(reversed(xrange(d)), xrange(d))]
+    if len(set(bl_tr_diag)) == 1 and bl_tr_diag[0] != "_":
+        return bl_tr_diag[0]
+    
+    # no winner
+    return None
 
 def game_over(board):
     if get_winner(board) is not None:
@@ -29,63 +57,56 @@ def game_over(board):
                 return False
     return True
 
-def get_winner(board):
-    for col in board:
-        if len(set(col)) == 1 and col[0] != "_":
-            return col[0]
-            
-    d = len(board)
-    rows = [[board[i][x] for i in xrange(d)] for x in xrange(d)]
-    for row in rows:
-        if len(set(row)) == 1 and row[0] != "_":
-            return row[0]
-            
-    tl_br_diag = [board[i][i] for i in xrange(d)]
-    if len(set(tl_br_diag)) == 1 and tl_br_diag[0] != "_":
-        return tl_br_diag[0]
-
-    bl_tr_diag = [board[i][j] for (i,j) in zip(reversed(xrange(d)), xrange(d))]
-    if len(set(bl_tr_diag)) == 1 and bl_tr_diag[0] != "_":
-        return bl_tr_diag[0]
-    return None
-
-# does not check that game is actually over
-# so might mistake incomplete for draw
+# returns 10 if @you is the winner, -10 if @you is the loser
+# and 0 otherwise (tie, or game not over)
 def evaluate(board, you):
     win = get_winner(board)
     if win is None:
         return 0
     return 10 if win == you else -10
     
-def minmax(board, player="o", opp="x", curr="o", lookAhead=6):
-    global ai_move
-
-    if game_over(board) or lookAhead < 0:
+def minmax(board, player="o", opp="x", curr="o", depth=9, alpha=-20, beta=20):
+    if game_over(board) or depth < 0:
         score = evaluate(board, player)
         return (score, None)
 
     d = len(board)
-    moves = [(y,x) for x in xrange(d) for y in xrange(d) if board[y][x] == "_"]
-    scores = []
+    moves = [(y,x) for y in xrange(d) for x in xrange(d) if board[y][x] == "_"]
     
+    move_to_return = None
     for move in moves:
         next_state = new_state(board, curr, move)
-        next = opp if curr == player else player
         score, _ = minmax(next_state, 
-                          curr=next,
-                          lookAhead=lookAhead - 1)
-        scores.append(score)
-        
-    if curr == player:
-        max_score = max(scores)
-        ai_move = moves[scores.index(max_score)]
-        return (max_score, ai_move)
-    else:
-        min_score = min(scores)
-        return (min_score, None)
+                          curr=opp if curr == player else player,
+                          depth= depth - 1,
+                          alpha=alpha, 
+                          beta=beta)
 
+        # alpha beta pruning
+        if curr == player:
+            if score > alpha:
+                alpha = score
+                move_to_return = move
+            if alpha >= beta:
+                break
+        else:
+            if score < beta:
+                beta = score
+            if alpha >= beta:
+                break
+    
+    # return either max or min, respectively
+    if curr == player:
+        return (alpha, move_to_return)
+    else:
+        return (beta, None)
+                
+
+
+#repl to play tictactoe against AI
 board = make_board(3)
-# repl to play tictactoe
+board[0][2] = "x"
+board[0][0] = "o"
 print "You are X"
 print "Enter your moves as: col row"
 while(True):
